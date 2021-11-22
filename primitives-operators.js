c9fa854b33b5b1fe5c22e0865f3edd5743a4390b
Angel Santana <angel.d.santana00@gmail.com>
2021-11-22 12:34:21 -0600

    exercise before lunch

---
+++ primitives-operators.js
@@ -0,0 +1,15 @@
+console.log(typeof 99.9)
+
+console.log(typeof "false")
+
+console.log(typeof false)
+
+console.log(typeof '0')
+
+console.log(typeof 0)
+
+console.log(typeof true)
+
+console.log(typeof 'true')
+
+
