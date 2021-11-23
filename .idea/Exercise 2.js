3a230c2adb719ecc18367a1858c2b87b450ca055
Angel Santana <angel.d.santana00@gmail.com>
2021-11-22 18:03:47 -0600

    exercise 2

---
+++ .idea/Exercise 2.js
@@ -0,0 +1,8 @@
+var sample = "Hello Codeup";
+    sample.length; 12
+    sample.toLowerCase();
+    //sample.toUpperCase();
+    sample += " Students";
+    sample.replace("Students", "Class");
+    sample.indexOf();
+console.log(sample);
