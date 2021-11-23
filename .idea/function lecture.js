a7545417e30ca6a7ad88085b3cda923470421932
Angel Santana <angel.d.santana00@gmail.com>
2021-11-23 12:20:42 -0600

    function lecture

---
+++ .idea/function lecture.js
@@ -0,0 +1,30 @@
+// function isTodayAGoodDay ( isCloudy, numberOfPlanes ) {
+//     if ( isCloudy && " numberOfPlanes < 10 " )
+//         console.log( "Delta is good to go");
+// else{
+//     console.log("Delta is a no go");
+// }
+// }
+//
+// isTodayAGoodDay (false, 9)
+
+function addition ( x, y ) {
+    return ( x + y );
+}
+console.log(addition)
+
+function subtraction ( x, y ) {
+    return ( x - y );
+}
+
+function multiply ( x, y ) {
+    return ( x * y );
+}
+
+function dividing ( x, y ) {
+    return ( x/y );
+}
+
+function dividingRemainder ( x, y ) {
+    return ( x%y )
+}
