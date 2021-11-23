4cb1648bac73e1fc1a90d6369e9dc840e2fb8ad7
Angel Santana <angel.d.santana00@gmail.com>
2021-11-22 18:26:20 -0600

    exercise 3

---
+++ .idea/Exercise 3.js
@@ -0,0 +1,22 @@
+//You have rented some movies for your kids:
+// The little mermaid (for 3 days),
+// Brother Bear (for 5 days, they love it),
+// and Hercules (1 day, you don't know yet if they're going to like it).
+// If price for a movie per day is $3, how much will you have to pay?
+1.
+var LittleMermaid = ( 3 * 3);
+var BrotherBear = ( 5 * 3);
+var Hercules = ( 1 * 3);
+console.log(LittleMermaid + BrotherBear + Hercules);
+
+2.
+var Google = (400 * 6);
+var Amazon = (380 * 4);
+var Facebook = (350 * 10);
+console.log(Google + Amazon + Facebook);
+
+3.
+var ClassNotFull = true
+var ClassDoesntConf = true
+var CanEnroll = ClassNotFull && ClassDoesntConf
+console.log
