fcfe031ed376025d84653adc7891bd4b0cdd6387
Angel Santana <angel.d.santana00@gmail.com>
2021-11-29 13:35:59 -0600

    conditional drills

---
+++ .idea/js/morningexercise.js
@@ -0,0 +1,55 @@
+//function reverseString(str) {
+  //  //return str.reverse();
+//}
+//console.log(reverseString("Angel"));
+
+//Morning Warm-UP
+
+//If Rainy, wear raincoat.
+//If Sunny, wear shirt.
+//If Snowy, wear hoodie.
+
+//let weather = "sunny";
+   // if (weather === "sunny") {
+    //    console.log("wear shirt")
+    //} else if (weather === "rainy"){
+    //    console.log("wear raincoat");
+    //} else if ()
+
+//
+//let myAge = 23;
+   // letActualAge = 21;
+   // function checkAge (myAge, actualAge) {
+    //    if (myAge === actualAge) {
+     //       console.log("myAge is my actual age")
+    //    }
+   // }
+
+//    checkAge();
+
+//let student1Grade = 99
+//let student2Grade = 75
+//let student3Grade = 66
+//let student4Grade = 84
+//let student5Grade = 52
+
+function checkGrades (grade) {
+    if (grade >= 90) {
+        console.log("Your grade is an A");
+    } else if (grade <= 89 && grade >=80) {
+        console.log("Your grade is a B");
+    } else if (grade <= 79 && grade >=70) {
+        console.log("Your grade is a C");
+    } else if (grade <= 69 && grade >=60) {
+        console.log("Your grade is a D");
+    } else if (grade <= 59 && grade >= 0) {
+        console.log("Your grade is an F")
+
+    }
+}
+//console.log(checkGrades (student5Grade))
+
+let student1Grade = (99) ? "Your grade is an A" : "Your grade is a B"
+
+
+
