a1d0637bc5ad1697113fff8f0084ec1ed35f4151
Angel Santana <angel.d.santana00@gmail.com>
2021-12-02 16:39:02 -0600

    for_loops completed

---
+++ .idea/js/for_loops.js
@@ -0,0 +1,41 @@
+
+/*
+
+function showMultiplicationTable (value) {
+    for ( let x = 1; x <= 10; x++ ){
+        console.log(value + "x" + x + "=" + (value * x))
+    }
+}
+showMultiplicationTable(9)
+*/
+///////////////////////////////////////
+
+/*for ( let x = 1; x <=10; x++) {
+    let i = Math.floor(Math.random() * (200 - 20) + 20);
+    if ( i % 2 === 0) {
+        console.log(i + " is an even number.");
+    } else {
+        console.log(i + " is an odd number.");
+    }
+}*/
+////////////////////////////////////////
+/*
+function numberPyramid(n) {
+    for (let i = 1; i <= 9; i++) {
+        let str = ''.repeat(n - i);
+
+        let str2 = i.toString().repeat(i)
+
+
+        console.log(str + str2);
+    }
+}
+
+numberPyramid(9)
+*/
+///////////////////////////////////////
+
+for ( let i = 100; i >= 5; i-=5 ) {
+    console.log(i)
+}
+
