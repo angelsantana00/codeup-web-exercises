f217691eaf85e48ae873153b71a2fa6422657863
Angel Santana <angel.d.santana00@gmail.com>
2021-12-02 17:19:32 -0600

    break_and_continue not completed

---
+++ .idea/js/break_and_continue.js
@@ -0,0 +1,10 @@
+let numberToSkipAt = 27
+
+    for (let i = 1; i <= 50; i += 2) {
+        if (i % 2 !== 0) {
+            console.log("Here is an odd number: " + i);
+        if (i === numberToSkipAt) {
+            console.log("Skippppppppppppppppppppppppppppppppppppppppp " + i);
+        }
+        }
+    }
