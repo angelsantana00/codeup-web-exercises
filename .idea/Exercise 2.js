0f61383c009aff4a73df162107a00edbf335623a
Angel Santana <angel.d.santana00@gmail.com>
2021-12-09 16:15:21 -0600

    organizing

--- .idea/Exercise 2.js
+++
@@ -1,8 +0,0 @@
-var sample = "Hello Codeup";
-    sample.length; 12
-    sample.toLowerCase();
-    //sample.toUpperCase();
-    sample += " Students";
-    sample.replace("Students", "Class");
-    sample.indexOf();
-console.log(sample);
--- .idea/exercise4.js
+++
@@ -1,5 +0,0 @@
-var username = 'codeup';
-var password = 'notastrongpassword';
-
-password.length >= 5;
-password.indexOf(username) === -1;
