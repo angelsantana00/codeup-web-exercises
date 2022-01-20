6ad1c46e2aec595268f369bde36fde7443f78b16
Angel Santana <angel.d.santana00@gmail.com>
2022-01-20 16:08:32 -0500

    jquery exercise selectors

---
+++ .idea/js/jquery_exercises.js
@@ -0,0 +1,10 @@
+// // alert($("#header").text());
+// let header1 = $("#header");
+// alert(header1.html());
+//
+// alert($("#paragraph").text());
+//
+// $(document).ready(function() {
+//     alert( 'The DOM has finished loading!' )});
+
+$(".codeup").css('border', '1px solid red');
